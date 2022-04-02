9c9ae6ec6c398d4b6e0d4e83a90e9bce7a894249
Devansh Mishra <79010144+devansh12b2@users.noreply.github.com>
2022-04-02 08:27:45 +0530

    flutter builder

--- lib/screen/notif.dart
+++ lib/screen/notif.dart
@@ -4,9 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sochem/main.dart';
 import 'package:sochem/models/notification_model.dart';
 import 'package:sochem/utils/constants.dart';
 import 'package:sochem/utils/endpoints.dart';
+import 'package:sochem/widgets/error_messages.dart';
 
 class Notif extends StatefulWidget {
   Notif({Key? key}) : super(key: key);
@@ -64,80 +66,91 @@ class _NotifState extends State<Notif> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        physics: ScrollPhysics(),
-        child: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.13,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10.0),
-                  bottomRight: Radius.circular(10.0),
-                ),
-                color: kPrimaryColor,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black54,
-                    offset: const Offset(1.0, 1.0), //Offset
-                    blurRadius: 10.0,
-                    spreadRadius: 1.0,
-                  ),
-                ],
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.13,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10.0),
+                bottomRight: Radius.circular(10.0),
               ),
-              child: Padding(
-                padding: EdgeInsets.only(top: 40.0),
-                child: Center(
-                  child: Text(
-                    'Notifications',
-                    style: GoogleFonts.raleway(
-                      textStyle: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                        // letterSpacing: 1.2,
-                      ),
+              color: kPrimaryColor,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black54,
+                  offset: const Offset(1.0, 1.0), //Offset
+                  blurRadius: 10.0,
+                  spreadRadius: 1.0,
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: EdgeInsets.only(top: 40.0),
+              child: Center(
+                child: Text(
+                  'Notifications',
+                  style: GoogleFonts.raleway(
+                    textStyle: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                      // letterSpacing: 1.2,
                     ),
                   ),
                 ),
               ),
             ),
-            ListView.builder(
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  shadowColor: int.parse(_allNotif[index].id) > oldId
-                      ? Colors.red
-                      : Colors.white,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 5,
-                    vertical: 8,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 30,
-                      child: FittedBox(
-                        child: Image.asset(
-                          notifEnums[_allNotif[index].type]!,
+          ),
+          FutureBuilder(
+            future: fetchNotifs(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) return SomethingWentWrong();
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        elevation: 5,
+                        shadowColor: int.parse(_allNotif[index].id) > oldId
+                            ? Colors.red
+                            : Colors.white,
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 5,
+                          vertical: 8,
                         ),
-                      ),
-                    ),
-                    title: Text(
-                      _allNotif[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      _allNotif[index].description,
-                    ),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 30,
+                            child: FittedBox(
+                              child: Image.asset(
+                                notifEnums[_allNotif[index].type]!,
+                              ),
+                            ),
+                          ),
+                          title: Text(
+                            _allNotif[index].title,
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          subtitle: Text(
+                            _allNotif[index].description,
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: _allNotif.length,
                   ),
                 );
-              },
-              itemCount: _allNotif.length,
-            ),
-          ],
-        ),
+              }
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Loading(),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
